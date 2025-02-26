5ff8e048847f0b3967bdc8895b164755aa7a5ce3
Peter Ott <ottpeter@tuta.io>
2025-02-26 18:43:13 +0100

    created backend folder

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+Create RSS feed from Polkadot proposals posted in the last 24hrs
---
+++ backend/src/refresh.ts
@@ -0,0 +1,52 @@
+import { Chain } from "./types/properties";
+import { createReferenda } from "./create";
+import { fetchDataFromAPI } from "./fetchReferendas";
+import { findNotionPageByPostId, getNotionPages } from "./findNotionPage";
+import { fetchDotToUsdRate, fetchKusToUsdRate } from "./utils/utils";
+
+const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+
+
+export async function refreshReferendas() {
+    try {
+        if (!notionDatabaseId) throw "Please specify REFRESH_INTERVAL in .env!";
+
+        // Fetch latest proposals from both networks, get list of Notion pages and fetch exchange rates
+        const [polkadotPosts, kusamaPosts, pages, dotUsdRate, kusUsdRate] = await Promise.all([
+            fetchDataFromAPI(30, Chain.Polkadot),
+            fetchDataFromAPI(30, Chain.Kusama),
+            getNotionPages(),
+            fetchDotToUsdRate(),
+            fetchKusToUsdRate()
+        ]);
+
+        // Combine them into one array
+        const referendas = [...polkadotPosts.referendas, ...kusamaPosts.referendas];
+
+        // Go through the fetched referendas
+        for (const referenda of referendas) {
+            // If Referenda exist in Notion, update it, otherwise, create new page
+            const found = await findNotionPageByPostId(pages, referenda.post_id);
+            const exchangeRate = referenda.network === Chain.Polkadot ?dotUsdRate : kusUsdRate;
+
+            if (found) {
+                console.log(`Proposal ${referenda.post_id} found in Notion.`);
+                try {
+                    // UPDATE
+                } catch (error) {
+                    console.error("Error updating referenda: ", (error as any).message);
+                }
+            } else {
+                console.log(`This proposal is not in Notion. ${referenda.post_id}`);
+                console.log(referenda.status)
+                try {
+                    await createReferenda(notionDatabaseId, referenda, exchangeRate, referenda.network);
+                } catch (error) {
+                    console.error("Error creating referenda: ", (error as any).message);
+                }
+            }
+        }
+    } catch (error) {
+        console.error("Error while refreshing Referendas: ", (error as any).message);
+    }
+}
