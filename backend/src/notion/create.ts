04f11c8dba0dbae93205d8648537798845ff5c76
Peter Ott <ottpeter@tuta.io>
2025-03-18 13:58:51 +0100

    Changes to new Notion schema at create as well

--- backend/src/notion/create.ts
+++ backend/src/notion/create.ts
@@ -23,12 +23,12 @@ export async function createReferenda(
 
   // Fill the properties, that are coming from Polkassembly
   const properties: CreateReferendumInput = {
-    name: referenda.title,
+    title: referenda.title,
     requestedAmount: rewardString,
     chain: network,
     origin: getValidatedOrigin(referenda.origin),
-    timeline: getValidatedStatus(referenda.status),
-    status: VoteStatus.NotStarted,
+    referendumTimeline: getValidatedStatus(referenda.status),
+    internalStatus: VoteStatus.NotStarted,
     link: `https://${network.toLowerCase()}.polkassembly.io/referenda/${referenda.post_id}`,
     number: referenda.post_id,
     created_at: referenda.created_at
@@ -65,10 +65,10 @@ function prepareNotionData(
   ): NotionCreatePageRequest {
     const properties: NotionProperties = {};
   
-    if (input.name) {
-      properties['Name'] = {
+    if (input.title) {
+      properties['Title'] = {
         type: 'title',
-        title: [{ text: { content: `#${input.number}-${input.name}` } }]
+        title: [{ text: { content: `#${input.number}-${input.title}` } }]
       };
     }
 
@@ -100,17 +100,17 @@ function prepareNotionData(
       };
     }
   
-    if (input.timeline) {
-      properties['Timeline'] = {
+    if (input.referendumTimeline) {
+      properties['Referendum timeline'] = {
         type: 'status',
-        status: { name: input.timeline }
+        status: { name: input.referendumTimeline }
       };
     }
   
-    if (input.status) {
-      properties['Status'] = {
+    if (input.internalStatus) {
+      properties['Internal status'] = {
         type: 'status',
-        status: { name: input.status }
+        status: { name: input.internalStatus }
       };
     }
 
