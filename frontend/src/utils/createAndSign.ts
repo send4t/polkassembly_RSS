d4a072991f7ba1fc6295376852b8266cfe1e48ba
Peter Ott <peter.ott@solarpunk.buzz>
2025-03-20 14:54:16 +0100

    Send request

--- frontend/src/utils/createAndSign.ts
+++ frontend/src/utils/createAndSign.ts
@@ -1,7 +1,6 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { web3Accounts, web3Enable, web3FromAddress } from "@polkadot/extension-dapp";
 import { encodeAddress, cryptoWaitReady, encodeMultiAddress } from "@polkadot/util-crypto";
-import { createTestKeyring } from '@polkadot/keyring';
 import { u8aToHex } from '@polkadot/util';
 import { ALICE, APP_NAME, BALANCE, BOB, KUSAMA_PROVIDER, PASEO_PROVIDER, SS58_FORMAT, THRESHOLD } from "./constants";
 import { AddressOrPair } from "@polkadot/api/types";
@@ -26,6 +25,9 @@ export async function createAndSign(
     //api.consts.system.ss58Prefix.toHuman()
     const senderAddress = encodeAddress(account.address, SS58_FORMAT);
     const injector = await web3FromAddress(senderAddress);
+    if (!injector || !injector.signer || !injector.signer.signRaw) {
+        throw new Error('Unable to create injector');
+    }
 
     /* Mimir Configuration */
     const config = {
@@ -41,28 +43,6 @@ export async function createAndSign(
     );
 
     // Prepare request payload
-
-    // Create signature
-
-    // Prepare final request
-
-    // Send request
-    
-    const maxWeight = {
-        refTime: api.createType('Compact<u64>', 2000000000),
-        proofSize: api.createType('Compact<u64>', 2000000)
-    };    
-
-    // Create the  multisig call
-    const threshold = 2;
-    const otherSignatories = multisig
-        .filter((address) => address !== senderAddress)
-        .sort();
-console.log("Other signatories: ", otherSignatories)
-    const maybeTimepoint = null;
-console.log("Api.txt ", api.tx)
-console.log(Object.keys(api.tx.convictionVoting));
-
     const call = api.tx.convictionVoting.vote(
         id,
         { Standard: {
@@ -70,50 +50,53 @@ console.log(Object.keys(api.tx.convictionVoting));
             balance: BALANCE
         }}
     );
+    const callHex = call.toHex();
 
-const balance = await api.query.system.account("Ht96h36AiW7x2PEEpR7Xkhxtue4KxfAccsMjHbHfDSi468R");
-console.log("Ht96h36AiW7x2PEEpR7Xkhxtue4KxfAccsMjHbHfDSi468R", balance.toHuman());
+    const payload = {
+        calldata: callHex,
+        timestamp: Date.now()
+    }
 
-console.log("Call JSON:", JSON.stringify(call.toHuman()));
-console.log("Call Hex:", call.toHex());
-const callHex = call.toHex();
-console.log("Encoded Call Hex:", callHex);
-const proxies = await api.query.proxy.proxies(senderAddress);
-console.log("Proxies:", proxies.toHuman());
+    // Create signature
+    const message = [
+        'Sign for mimir batch\n',
+        `Call Data: ${payload.calldata}\n`,
+        `Address: ${multisigAddress}\n`,
+        `Timestamp: ${payload.timestamp}`
+    ].join('');
 
-    
-    // Create the multisig transaction
-    const multisigCall = api.tx.multisig.asMulti(
-        threshold,
-        otherSignatories,
-        maybeTimepoint,
-        call,
-        maxWeight
-    );
+    const signature = await injector.signer.signRaw({
+        address: senderAddress,
+        data: u8aToHex(Buffer.from(message, 'utf-8')),
+        type: 'bytes'
+    });
+    const signatureHex = signature.signature;
 
+    // Prepare final request
+    const request = {
+        ...payload,
+        signature: signatureHex,
+        signer: senderAddress
+    };
+
+    // Send request
     try {
-        const txHash = await multisigCall.signAndSend(
-            senderAddress, 
-            { signer: injector.signer }
-        );
-
-        
-        findTxBlock(api, txHash.toHex()).then((result) => {
-            if (result) {
-                console.log(`Transaction included in block ${result.blockNumber} at index ${result.index}`);
-
-                const url = `http://localhost:3000/sign?network=${network}&refid=${id}&multisig=${multisig}&height=${result.blockNumber}&index=${result.index}`;
-                console.log("Url: ", url);
-            } else {
-                console.log("Transaction not found within the given retries.");
+        const response = await fetch(
+            `${config.clientGateway}/v1/chains/${config.chain}/${multisigAddress}/transactions/batch`,
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(request)
             }
-        }).catch(console.error);
-
-        
-
-        console.log('Transaction submitted with hash:', txHash.toHex());
+        );
+      
+        const result = await response.json();
+        return result;
     } catch (error) {
-        console.error('Transaction failed:', error);
+        console.error('Failed to upload transaction: ', error);
+        throw error;
     }
 }
 
